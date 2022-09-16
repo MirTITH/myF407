1a023a6c8042ce6ab6e4f811de2b33989e22ba48
TITH <1023515576@qq.com>
2022-09-16 17:03:28 +0800

    添加命令行

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -24,7 +24,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "user_main.h"
+#include "user_main.hpp"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
--- UserCode/user_main.cpp
+++ UserCode/user_main.cpp
@@ -1,64 +1,18 @@
+#include "user_main.hpp"
 #include "main.h"
 #include "cmsis_os.h"
-#include <iostream>
-#include "user_main.h"
+#include "stdio_CLI.h"
 
 using namespace std;
-
-
-void Thread(void const *argument)
-{
-    const osThreadDef_t *this_thread = (const osThreadDef_t *)argument;
-    double dnum                      = 0;
-
-    for (;;) {
-        // taskENTER_CRITICAL();
-        cout << this_thread->name << ": "<< dnum << endl;
-        dnum += 0.1;
-        // taskEXIT_CRITICAL();
-        osDelay(100);
-    }
-}
 
 void StartDefaultTask(void const *argument)
 {
     (void)argument;
 
-    osThreadDef_t t1 = {
-        .name         = (char *)"thread1",
-        .pthread      = Thread,
-        .tpriority    = osPriorityNormal,
-        .instances    = 0,
-        .stacksize    = 256,
-        .buffer       = NULL,
-        .controlblock = NULL};
-
-    osThreadCreate(&t1, &t1);
-
-        osThreadDef_t t2 = {
-        .name         = (char *)"thread2",
-        .pthread      = Thread,
-        .tpriority    = osPriorityNormal,
-        .instances    = 0,
-        .stacksize    = 256,
-        .buffer       = NULL,
-        .controlblock = NULL};
-
-    osThreadCreate(&t2, &t2);
-
-        osThreadDef_t t3 = {
-        .name         = (char *)"thread3",
-        .pthread      = Thread,
-        .tpriority    = osPriorityBelowNormal,
-        .instances    = 0,
-        .stacksize    = 256,
-        .buffer       = NULL,
-        .controlblock = NULL};
-
-    osThreadCreate(&t3, &t3);
+    CLI_Start();
 
     for (;;) {
         HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_6);
-        osDelay(200);
+        osDelay(100);
     }
 }
